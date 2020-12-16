d8b4b290b54ca8465d82544254bc9b487e2f2ee0
Jurian Baas <jurian.baas@gmail.com>
2020-12-16 16:14:04 +0100

    Added routes to pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,40 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+
+import Profiel from "../views/Profiel.vue";
+import Haltes from "../views/Haltes.vue";
+import Kaart from "../views/Kaart.vue";
+import Halteknop from "../views/Halteknop.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Haltes",
+    component: Haltes,
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: "/profiel",
+    name: "Profiel",
+    // route level code-splitting
+    // this generates a separate chunk (profiel.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "profiel" */ "../views/Profiel.vue"),
+  },
+  {
+    path: "/kaart",
+    name: "Kaart",
+    component: () =>
+      import(/* webpackChunkName: "kaart" */ "../views/Kaart.vue"),
+  },
+  {
+    path: "/halteknop",
+    name: "Halteknop",
+    component: () =>
+      import(/* webpackChunkName: "halteknop" */ "../views/Halteknop.vue"),
+  },
 ];
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,36 @@
 <template>
   <div>
     <p><a @click="sheet = true">toggle</a></p>
-    <v-bottom-sheet v-model="sheet" hide-overlay
-      >Whaaaaat <br /><br /><br /><br /><br />
-      iets</v-bottom-sheet
-    >
-    <ul></ul>
-
-    <li
-      v-for="(item, index) in quays
-        .filter((el) => el.stopplacename.publicname.includes('plein'))
-        .map((el) => el.stopplacename.publicname)"
-      :key="index"
-    >
-      {{ item }}
-    </li>
+    <v-bottom-sheet v-model="sheet" hide-overlay>
+      <div class="pl-2">
+        <h1>Heading</h1>
+        <br /><br /><br /><br /><br />
+        iets
+      </div>
+    </v-bottom-sheet>
+    <h1>Heading</h1>
+    <ul>
+      <li
+        v-for="(item, index) in quays
+          .filter((el) => el.stopplacename.publicname.includes('plein'))
+          .map((el) => el.stopplacename.publicname)"
+        :key="index"
+      >
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
 
+<style lang="scss" scoped>
+h1 {
+  color: var(--v-secondary-base);
+}
+</style>
+
 <style>
 .v-bottom-sheet {
-  /* height: 600px; */
-  background-color: white;
-}
-
-.v-bottom-sheet:before {
-  content: "";
-  width: 36px;
-  height: 6px;
-  position: absolute;
-  left: 50%;
-  margin-left: -18px;
-  margin-top: 6px;
-  border-radius: 3px;
-  background: #666;
+  background-color: white !important;
 }
 </style>
 
