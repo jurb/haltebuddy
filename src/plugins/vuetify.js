c2fb40d0643a732ad63ab6e7156f1990de30d276
Jurian Baas <jurian.baas@gmail.com>
2020-12-16 16:13:17 +0100

    Adjusted theme and enabled customproperties so we can use vars in components

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,31 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    options: { customProperties: true },
+    themes: {
+      light: {
+        primary: {
+          base: "#009ee3", // GVB cyaan
+          lighten1: "#d3dff3",
+        },
+        secondary: {
+          base: "#0863b5", // GVB blauw
+          lighten1: "#d4edfc",
+        },
+        accent: {
+          base: "#f39100", // Oranje
+          lighten1: "#feead3",
+        },
+        error: "#e3001f", // Rood
+        "gvb-yellow": "#fec600",
+        "gvb-pink": "#e50064",
+        "gvb-green": "#13a538",
+        "gvb-purple": "#fec600",
+      },
+    },
+  },
 });
