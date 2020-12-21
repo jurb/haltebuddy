9bf048d8776211523f3dae5f0a852d91fa3c3ed4
Jurian Baas <jurian.baas@gmail.com>
2020-12-21 13:19:15 +0100

    add quay-card component

---
+++ src/components/QuayCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>haltekaartje: {{ quay.stopplace.stopplacecode }}</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    value: "",
+    //
+  }),
+  props: ["quay"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Haltes.vue
+++ src/views/Haltes.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
+    <!-- <h1>Haltes</h1> -->
     <search-bar />
+    <quay-card
+      v-for="(quay, index) in filteredQuays
+        .filter((el) => el.stopplace.stopplacename.publicname)
+        .filter((el) =>
+          el.stopplace.stopplacename.publicname.includes('plein')
+        )"
+      :key="index"
+      :quay="quay"
+    />
     <ul>
       <li
         v-for="(item, index) in filteredQuays
@@ -21,6 +31,8 @@
 <script>
 import { mapGetters, mapState } from "vuex";
 import SearchBar from "@/components/SearchBar.vue";
+import QuayCard from "@/components/QuayCard.vue";
+
 export default {
   name: "Haltes",
   computed: {
@@ -30,7 +42,10 @@ export default {
   data: () => ({
     test: false,
   }),
+  components: {
     SearchBar,
+    QuayCard,
+  },
 };
 </script>
 
