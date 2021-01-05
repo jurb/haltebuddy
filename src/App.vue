903f820a6b7c810fc039c8480ee55f90a5fb0e18
Jurian Baas <jurian.baas@gmail.com>
2021-01-05 14:45:30 +0100

    remove keep-alive for performance (and I don't understand it yet)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
   <v-app>
-    <!-- <v-navigation-drawer app>
-    </v-navigation-drawer> -->
-
-    <!-- <v-app-bar app>
-    </v-app-bar> -->
-
-    <!-- Sizes your content based upon application components -->
     <v-main>
-      <!-- Provides the application the proper gutter -->
       <v-container fluid>
-        <keep-alive>
-          <router-view></router-view>
-        </keep-alive>
+        <router-view></router-view>
       </v-container>
     </v-main>
-
     <v-footer app>
       <bottom-navigation />
     </v-footer>
   </v-app>
 </template>
-
 <script>
 // @ is an alias to /src
 import BottomNavigation from "@/components/BottomNavigation.vue";
