4d349842ae66f0e4a412015e488ca808d634b87f
Jurian Baas <jurian.baas@gmail.com>
2021-01-11 12:08:00 +0100

    import quays directly from json, moved cleanup to node script

--- src/store/index.js
+++ src/store/index.js
@@ -6,31 +6,12 @@ import { profileAccessibleScore } from "@/helpers/profileAccessibleScore";
 
 import stops from "@/assets/data/ExportCHBLatest-amsterdam.json";
 
+import quaysImport from "@/assets/data/ExportCHBLatest-quays-selection-amsterdam.json";
+
 // TODO: we might want to alter the quays in the node script for performance and seperation of concerns
-const quays = stops
-  .map((stopplace) => {
-    const { quays, ...stop } = stopplace; // destructure quays and stop objects in own constants
-    const quaysWrapped =
-      { ...quays }["quay"] instanceof Array
-        ? { ...quays }["quay"]
-        : [{ ...quays }["quay"]]; // if there is a single quay object, put it in an array so we can map over it
-
-    const quaysWithStop = quaysWrapped
-      .filter((el) => el) // remove empty arrays
-      .map((quay) => ({
-        // map over each quay
-        ...quay, // destructure whole quay object
-        stopplace: stop, // also add the stopplace information
-        geo: rdToWgs84(
-          quay.quaylocationdata["rd-x"],
-          quay.quaylocationdata["rd-y"]
-        ), // add wsg84 coordinates
-      }));
-
-    return quaysWithStop;
-  })
-  .flat()
-  .filter((d) => d.quaystatusdata.quaystatus == "available"); // flatten nested quay arrays
+const quays = quaysImport.filter(
+  (d) => d.quaystatusdata.quaystatus == "available"
+); // flatten nested quay arrays
 
 Vue.use(Vuex);
 
@@ -53,13 +34,10 @@ export const store = new Vuex.Store({
           .map((quay) => ({
             ...quay,
             profileAccessibleScore: profileAccessibleScore(quay, state.profile),
-            direction: getCardinalDirectionShort(
-              quay.quaybearing.compassdirection
-            ),
           }))
           // I'm filtering the initial results for now, performance of the Halte
           // component isn't great if we show all quays
-          .filter((el) => el.quaynamedata.quayname.includes("plein"))
+          .filter((el) => el.quaynamedata.quayname.includes("kade"))
           .sort((a, b) =>
             a.quaynamedata.quayname.localeCompare(b.quaynamedata.quayname)
           )
