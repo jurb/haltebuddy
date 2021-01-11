850d63828a7bc69265e2f43e8c953c006b6ad5d3
Jurian Baas <jurian.baas@gmail.com>
2021-01-11 13:50:14 +0100

    swap labels and rating icon

--- src/components/QuayListItem.vue
+++ src/components/QuayListItem.vue
@@ -1,125 +1,116 @@
 <template>
   <div>
-    <v-lazy
-      v-model="isActive"
-      :options="{
-        threshold: 0.5,
-      }"
-      min-height="100"
-      transition-group="fade-transition"
-    >
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title
-            >{{ quay.stopplace.stopplacename.publicname }} –
-            {{ quay.quaytransportmodes.transportmodedata.transportmode }}
-            {{ quay.direction }}
-            <span class="float-right"
-              >{{
-                ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"][
-                  Math.floor(Math.random() * 9)
-                ]
-              }}{{
-                ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"][
-                  Math.floor(Math.random() * 9)
-                ]
-              }}</span
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title
+          >{{ quay.stopplace.stopplacename.publicname }} –
+          {{ quay.quaytransportmodes.transportmodedata.transportmode }}
+          {{ quay.direction }}
+          <span class="float-right"
+            >{{
+              ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"][
+                Math.floor(Math.random() * 9)
+              ]
+            }}{{
+              ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"][
+                Math.floor(Math.random() * 9)
+              ]
+            }}</span
+          >
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          <v-rating
+            color="secondary"
+            background-color="secondary lighten-1"
+            dense
+            hover
+            length="3"
+            size="16"
+            :value="quay.profileAccessibleScore.overallRating"
+          ></v-rating>
+        </v-list-item-subtitle>
+        <v-container fluid class="px-0">
+          <v-row no-gutters>
+            <v-col
+              v-if="!quay.profileAccessibleScore.threshold"
+              align-self="start"
+              class="px-0"
+              align="center"
             >
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            <v-rating
-              color="secondary"
-              background-color="secondary lighten-1"
-              dense
-              hover
-              length="3"
-              size="16"
-              :value="quay.profileAccessibleScore.overallRating"
-            ></v-rating>
-          </v-list-item-subtitle>
-          <v-container fluid class="px-0">
-            <v-row no-gutters>
-              <v-col
-                v-if="!quay.profileAccessibleScore.threshold"
-                align-self="start"
-                class="px-0"
-                align="center"
-              >
-                <img :src="require('@/assets/icons/quayNoThreshold.svg')" />
-                <p class="caption">{{ ratingSymbol(3) }} Opgang</p></v-col
-              >
-              <v-col
-                v-if="quay.profileAccessibleScore.threshold"
-                align-self="start"
-                class="px-0"
-                align="center"
-              >
-                <img :src="require('@/assets/icons/quayThreshold.svg')" />
-                <p class="caption">
-                  {{
-                    ratingSymbol(
-                      quay.profileAccessibleScore.stopThresholdRating
-                    ) || "?"
-                  }}
-                  Drempel<br />
-                  {{
-                    Math.round(quay.profileAccessibleScore.stopThreshold * 100)
-                  }}cm
-                </p></v-col
-              >
-              <v-col align-self="start" class="px-0" align="center">
-                <img :src="require('@/assets/icons/quayWidth.svg')" />
-                <p class="caption">
-                  {{
-                    quay.profileAccessibleScore.stopNarrowestWidth
-                      ? `${ratingSymbol(
-                          quay.profileAccessibleScore.stopNarrowestWidthRating
-                        )}
-                ${quay.profileAccessibleScore.stopNarrowestWidth}m
+              <img :src="require('@/assets/icons/quayNoThreshold.svg')" />
+              <p class="caption">{{ ratingSymbol(3) }} Opgang</p></v-col
+            >
+            <v-col
+              v-if="quay.profileAccessibleScore.threshold"
+              align-self="start"
+              class="px-0"
+              align="center"
+            >
+              <img :src="require('@/assets/icons/quayThreshold.svg')" />
+              <p class="caption">
+                {{
+                  Math.round(quay.profileAccessibleScore.stopThreshold * 100)
+                }}cm
+                {{
+                  ratingSymbol(
+                    quay.profileAccessibleScore.stopThresholdRating
+                  ) || "?"
+                }}
+              </p></v-col
+            >
+            <v-col align-self="start" class="px-0" align="center">
+              <img :src="require('@/assets/icons/quayWidth.svg')" />
+              <p class="caption">
+                {{
+                  quay.profileAccessibleScore.stopNarrowestWidth
+                    ? ` ${
+                        quay.profileAccessibleScore.stopNarrowestWidth
+                      }m ${ratingSymbol(
+                        quay.profileAccessibleScore.stopNarrowestWidthRating
+                      )}
+
                  `
-                      : "Breedte onbekend"
-                  }}
-                </p>
-              </v-col>
-              <v-col
-                v-if="profile.ramp"
-                align-self="start"
-                class="px-0"
-                align="center"
-              >
-                <img :src="require('@/assets/icons/quayRamp.svg')" />
-                <p class="caption">
-                  {{ ratingSymbol(quay.profileAccessibleScore.rampRating) }}
-                  Plank
-                </p>
-              </v-col>
-              <v-col
-                v-if="!profile.ramp"
-                align-self="start"
-                class="px-0"
-                align="center"
-              >
-                <img
-                  :src="require('@/assets/icons/quayThresholdToVehicle.svg')"
-                />
-                <p class="caption">
-                  {{
-                    ratingSymbol(
-                      quay.profileAccessibleScore.vehicleThresholdRating
-                    )
-                  }}
-                  {{
-                    Math.round(
-                      quay.profileAccessibleScore.vehicleThreshold * 100
-                    )
-                  }}cm
-                </p>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-list-item-content>
-      </v-list-item>
-    </v-lazy>
+                    : "Breedte onbekend"
+                }}
+              </p>
+            </v-col>
+            <v-col
+              v-if="profile.ramp"
+              align-self="start"
+              class="px-0"
+              align="center"
+            >
+              <img :src="require('@/assets/icons/quayRamp.svg')" />
+              <p class="caption">
+                Plank {{ ratingSymbol(quay.profileAccessibleScore.rampRating) }}
+              </p>
+            </v-col>
+            <v-col
+              v-if="!profile.ramp"
+              align-self="start"
+              class="px-0"
+              align="center"
+            >
+              <img
+                :src="require('@/assets/icons/quayThresholdToVehicle.svg')"
+              />
+              <p class="caption">
+                {{
+                  Math.round(
+                    quay.profileAccessibleScore.vehicleThreshold * 100
+                  )
+                }}cm
+                {{
+                  ratingSymbol(
+                    quay.profileAccessibleScore.vehicleThresholdRating
+                  )
+                }}
+              </p>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-list-item-content>
+    </v-list-item>
     <v-divider></v-divider>
   </div>
 </template>
