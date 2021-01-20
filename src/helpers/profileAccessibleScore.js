93b8509ebdc2ca76f5f6edc76f4505aae2f852a8
Jurian Baas <jurian.baas@gmail.com>
2021-01-20 13:44:14 +0100

    refactored rating helper, constants on top and better comments

--- src/helpers/profileAccessibleScore.js
+++ src/helpers/profileAccessibleScore.js
@@ -3,79 +3,113 @@ import * as d3 from "d3-scale";
 // TODO: M&E: move all constants on top
 
 function profileAccessibleScore(quay, profile) {
-  const stopRatingRange = [0, 1, 2, 3];
+  // our rating range (higher is better) and a derived maximum score
+  const RATINGRANGE = [0, 1, 2, 3];
+  const maximumRating = RATINGRANGE.slice(-1)[0];
 
-  const stopRatingScale = (domain) =>
+  // our rating scale function and domain constants. Calling ratingScale(domain)(number) will output
+  // a number from RATINGRANGE, depending on where in the domain the number is.
+  // e.g. ratingScale(THRESHOLDDOMAIN)(-0.2) will output '0'
+  // and ratingScale(STOPNARROWESTWIDTHDOMAIN)(0.01) will output '2'
+  const ratingScale = (domain) =>
     d3
       .scaleThreshold()
       .domain(domain)
-      .range(stopRatingRange);
+      .range(RATINGRANGE);
+  // domain constants
+  const THRESHOLDDOMAIN = [-0.01, 0, 0.01];
+  const STOPNARROWESTWIDTHDOMAIN = [-0.05, 0, 0.05];
+  const RAMPROOMWIDTHDOMAIN = [-0.2, 0, 0.05];
+  const RAMPROOMMINHEIGHTDOMAIN = [-0.01, 0, 0];
+  const VEHICLETHRESHOLDDOMAIN = [-0.02, 0, 0.01];
 
+  // we subtract 1cm from the 30cm of a tram due to wear
+  const VEHICLETRAMHEIGHT = 0.29;
+  const VEHICLEBUSHEIGHT = 0.23;
+
+  // when using the ramp, we think wheelchair users need a quay width of at least 1.5 meters to be able to board
+  const RAMPWIDTHNEEDED = 1.5;
+
+  const WHEELCHAIRMANUAL = "wheelchairManual";
+  const WHEELCHAIRELECTRIC = "wheelchairElectric";
+
+  // Ramp min-height values are calculated by the maximum angle the ramp can have,
+  // see https://docs.google.com/spreadsheets/d/1ANc_PizZm7Eu82_2hiIWGCYqAqQ7Wi2IE11AMYkdxq0/edit?usp=sharing
+  const MINHEIGHT_MANUAL_BUS = 0.161;
+  const MINHEIGHT_ELECTRIC_BUS = 0.126;
+  const MINHEIGHT_MANUAL_TRAM = 0.221;
+  const MINHEIGHT_ELECTRIC_TRAM = 0.186;
+
+  // Threshold rating block
   const threshold =
     !quay.ramp && !quay.stopplaceaccessroute && quay.kerbheight > 0;
-
-  const transportMode = quay.transportmode;
-
   const stopThresholdProfile = profile.threshold / 100;
   const stopThreshold = threshold ? quay.kerbheight : null;
   const stopThresholdDifference = stopThresholdProfile - stopThreshold;
   // voorbeeld: profiel: maxdrempel is 5, situatie: drempel is 6 => difference 5-6= -1
   const stopThresholdRating = threshold
-    ? stopRatingScale([-0.01, 0, 0.01])(stopThresholdDifference)
-    : 3;
+    ? ratingScale(THRESHOLDDOMAIN)(stopThresholdDifference)
+    : maximumRating;
 
+  // stopNarrowestWidthRating block
   const stopNarrowestWidthProfile = profile.width / 100;
   const stopNarrowestWidth = quay.narrowestpassagewidth;
   const stopNarrowestWidthDifference =
     stopNarrowestWidth - stopNarrowestWidthProfile;
-  const stopNarrowestWidthRating = stopRatingScale([-0.05, 0, 0.05])(
+  const stopNarrowestWidthRating = ratingScale(STOPNARROWESTWIDTHDOMAIN)(
     stopNarrowestWidthDifference
   );
 
+  // vehicleThresholdRating block
+  const transportMode = quay.transportmode;
   const vehicleThresholdProfile = profile.threshold / 100;
   const vehicleThreshold =
     transportMode === "tram"
-      ? 0.29 - quay.kerbheight
+      ? VEHICLETRAMHEIGHT - quay.kerbheight
       : transportMode === "bus"
-      ? 0.23 - quay.kerbheight
+      ? VEHICLEBUSHEIGHT - quay.kerbheight
       : null;
   const vehicleThresholdDifference = vehicleThresholdProfile - vehicleThreshold;
   // give this the highest rating if user needs a ramp
   const vehicleThresholdRating = profile.ramp
-    ? 3
-    : stopRatingScale([-0.02, 0, 0.01])(vehicleThresholdDifference);
+    ? maximumRating
+    : ratingScale(VEHICLETHRESHOLDDOMAIN)(vehicleThresholdDifference);
 
-  const rampRoomWidthProfile = 1.5;
+  // rampRoomWidthRating block
+  const rampRoomWidthProfile = RAMPWIDTHNEEDED;
   const rampRoomWidth = quay.boardingpositionwidth;
   const rampRoomWidthDifference = rampRoomWidth - rampRoomWidthProfile;
   // give this the highest rating if user does not need a ramp
   const rampRoomWidthRating = profile.ramp
-    ? stopRatingScale([-0.2, 0, 0.05])(rampRoomWidthDifference)
-    : 3;
+    ? ratingScale(RAMPROOMWIDTHDOMAIN)(rampRoomWidthDifference)
+    : maximumRating;
 
-  // TODO: rampRoomMinHeight lostrekken in tram en bus variant, en nog rekening houden met afmetingen ramp bus, dat doen we nu nog niet
+  // rampRoomMinHeightRating block
+  // TODO: we don't take the different ramp dimensions of tram and bus into account
+  // TODO: add checks for metro and ferry?
   const rampRoomMinHeightProfile =
-    profile.profileModality === "wheelchairManual" && transportMode === "bus"
-      ? 0.161
-      : profile.profileModality === "wheelchairElectric" &&
+    profile.profileModality === WHEELCHAIRMANUAL && transportMode === "bus"
+      ? MINHEIGHT_MANUAL_BUS
+      : profile.profileModality === WHEELCHAIRELECTRIC &&
         transportMode === "bus"
-      ? 0.126
-      : profile.profileModality === "wheelchairManual" &&
+      ? MINHEIGHT_ELECTRIC_BUS
+      : profile.profileModality === WHEELCHAIRMANUAL && transportMode === "tram"
+      ? MINHEIGHT_MANUAL_TRAM
+      : profile.profileModality === WHEELCHAIRELECTRIC &&
         transportMode === "tram"
-      ? 0.221
-      : profile.profileModality === "wheelchairElectric" &&
-        transportMode === "tram"
-      ? 0.186
-      : null;
+      ? MINHEIGHT_ELECTRIC_TRAM
+      : // for now we throw a null for other transportmodes, leading to a max rating
+        null;
   const rampRoomKerbHeight = quay.kerbheight;
   const rampRoomMinHeightDifference =
     rampRoomKerbHeight - rampRoomMinHeightProfile;
-  // TODO: zie hierboven, ratingschaal moet rekening houden met bus en tram, dit is nog te kort door de bocht
+  // TODO: maybe there should be a difference in the rating for a bus and a tram quay here
   // give this the highest rating if user does not need a ramp
   const rampRoomMinHeightRating = profile.ramp
-    ? stopRatingScale([-0.01, 0, 0])(rampRoomMinHeightDifference)
-    : 3;
+    ? ratingScale(RAMPROOMMINHEIGHTDOMAIN)(rampRoomMinHeightDifference)
+    : maximumRating;
 
+  // we use a combined ramp rating
   const rampRating = Math.min(rampRoomMinHeightRating, rampRoomWidthRating);
 
   const allRatings = [
@@ -87,15 +121,11 @@ function profileAccessibleScore(quay, profile) {
     rampRating,
   ];
 
-  const overallRating = Math.min(
-    stopThresholdRating,
-    stopNarrowestWidthRating,
-    vehicleThresholdRating,
-    rampRoomWidthRating,
-    rampRoomMinHeightRating,
-    rampRating
-  );
+  // the overall rating is the lowest rating of all ratings
+  const overallRating = Math.min(...allRatings);
 
+  // to return all calculated constands, we construct them in an object here
+  // TODO: is there a better way?
   return {
     transportMode,
     threshold,
