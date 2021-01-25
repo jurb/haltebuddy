f8249113610657c42b890a72137436e1cc1834ca
Jurian Baas <jurian.baas@gmail.com>
2021-01-25 14:01:56 +0100

    call second pdok api for setting location result

--- src/components/AddressAutoComplete.vue
+++ src/components/AddressAutoComplete.vue
@@ -1,19 +1,24 @@
 <template>
-  <v-autocomplete
-    v-model="model"
-    :items="filteredItems"
-    :loading="isLoading"
-    :search-input.sync="search"
-    clearable
-    item-text="weergavenaam"
-    item-value="weergavenaam"
-    label="Zoek adres"
-    no-data-text="Niks gevonden"
-    :hide-no-data="true"
-    no-filter
-    solo
+  <div>
+    <v-autocomplete
+      v-model="model"
+      :items="filteredItems"
+      :loading="isLoading"
+      :search-input.sync="search"
+      clearable
+      item-text="weergavenaam"
+      item-value="id"
+      label="Zoek adres"
+      no-data-text="Niks gevonden"
+      :hide-no-data="true"
+      no-filter
+      solo
       class="px-4 pt-4"
     >
+      <template v-slot:append-outer> <location-set-icon /> </template>
+    </v-autocomplete>
+    <v-divider />
+  </div>
 </template>
 
 <script>
@@ -30,13 +35,6 @@ export default {
   }),
   computed: {
     ...mapState(["currentLocation"]),
-    locationSelectedItem: function() {
-      const coordsFlipped = this.items[0].centroide_ll
-        .replace("POINT(", "")
-        .replace(")", "")
-        .split(" ");
-      return [Number(coordsFlipped[1]), Number(coordsFlipped[0])];
-    },
     filteredItems: function() {
       // remove postal code and city name from results, since we're focussing on Amsterdam right now.
       // for national results just point the autocomplete component to this.items instead
@@ -48,14 +46,12 @@ export default {
   },
   methods: {
     ...mapActions(["changeCurrentLocation"]),
-  },
-  watch: {
-    search(val) {
+    searchSuggestAPI: function(val) {
       // Items have already been loaded
       this.isLoading = true;
       // Lazily load input items
       fetch(
-        `https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?fq=type:weg OR type:adres&fl=*&q=${this.search} AND woonplaatsnaam:Amsterdam`
+        `https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?fq=type:weg OR type:adres&q=${val} AND woonplaatsnaam:Amsterdam`
       )
         .then((res) => res.json())
         .then((res) => {
@@ -66,8 +62,39 @@ export default {
         })
         .finally(() => (this.isLoading = false));
     },
-    model() {
-      this.model ? this.changeCurrentLocation(this.locationSelectedItem) : "";
+    convertPointString: function(pointstring) {
+      // API return PostGIS like string of coordinates, convert to array of numbers and reverse it
+      return pointstring
+        .replace("POINT(", "")
+        .replace(")", "")
+        .split(" ")
+        .map(Number)
+        .slice()
+        .reverse();
+    },
+    setLocationFromResult: function(id) {
+      this.isLoading = true;
+      fetch(
+        `https://geodata.nationaalgeoregister.nl/locatieserver/v3/lookup?id=${id}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          const centroide_ll = res.response.docs[0].centroide_ll;
+          const coords = this.convertPointString(centroide_ll);
+          this.changeCurrentLocation(coords);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => (this.isLoading = false));
+    },
+  },
+  watch: {
+    search(val) {
+      this.searchSuggestAPI(val);
+    },
+    model(val) {
+      this.setLocationFromResult(val);
     },
   },
 };
