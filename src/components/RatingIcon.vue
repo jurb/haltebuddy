6e59760c6b686a036019985a186e6773c60f64f2
Jurian Baas <jurian.baas@gmail.com>
2021-01-26 09:35:54 +0100

    ratingicon component

---
+++ src/components/RatingIcon.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-btn fab x-small depressed :color="scoreColor" class="icon-round">
+    <v-icon>
+      {{ scoreIcon }}
+    </v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "RatingIcon",
+  props: ["rating"],
+  computed: {
+    scoreColor: function() {
+      return this.rating === 0
+        ? "error"
+        : this.rating === 1
+        ? "accent"
+        : this.rating === 2
+        ? "gvb-green lighten-2"
+        : this.rating === 3
+        ? "gvb-green"
+        : "error";
+    },
+    scoreIcon: function() {
+      return this.rating === 0
+        ? "mdi-close"
+        : this.rating === 1
+        ? "mdi-exclamation"
+        : this.rating === 2
+        ? "mdi-check"
+        : this.rating === 3
+        ? "mdi-check"
+        : "mdi-help";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// Here I hack vuetify's button to create a round icon, and remove the pointer event. A svg icon is probably better
+.icon-round {
+  pointer-events: none;
+}
+</style>
