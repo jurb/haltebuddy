64b9338e518fb3d1621c0d5d5caabc44b4f970e6
Jurian Baas <jurian.baas@gmail.com>
2021-01-28 10:27:32 +0100

    WIP: quays on map

--- src/views/QuayMap.vue
+++ src/views/QuayMap.vue
@@ -2,6 +2,7 @@
   <mapbox
     access-token="pk.eyJ1IjoianVyaWFuIiwiYSI6ImNqNTJlMTAzYzBmajEyd242ank0ZGppam8ifQ.JlOhR91CJQnc3U12pbMQwQ"
     :map-options="mapOptions"
+    @map-load="loaded"
   />
 </template>
 
@@ -14,14 +15,45 @@ export default {
   components: { Mapbox },
   computed: {
     ...mapState(["profile", "quays"]),
-    ...mapGetters(["filterQuays"]),
+    ...mapGetters(["filteredQuays"]),
+    filterQuaysGeoJSON: function() {
+      return {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: this.filteredQuays.map((d) => ({
+            type: "Feature",
+            id: d.quaycode,
+            geometry: { type: "Point", coordinates: [d.geo.lon, d.geo.lat] },
+            properties: {
+              ...d,
+            },
+          })),
+        },
+      };
+    },
+  },
+  methods: {
+    loaded(map) {
+      map.addLayer({
+        id: "quays",
+        source: this.filterQuaysGeoJSON,
+        type: "circle",
+        paint: {
+          "circle-radius": 4,
+          "circle-color": "green",
+          "circle-opacity": 0.6,
+          "circle-stroke-width": 0,
+        },
+      });
+    },
   },
   data: () => ({
     test: false,
     mapOptions: {
       style: "mapbox://styles/mapbox/light-v9",
-      center: [-96, 37.8],
-      zoom: 3,
+      center: [4.9, 52.366667],
+      zoom: 12,
     },
   }),
 };
