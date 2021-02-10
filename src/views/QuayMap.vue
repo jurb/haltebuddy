e891fdd79fb99ad2c24b45d93d0a017ceac61186
Jurian Baas <jurian.baas@gmail.com>
2021-02-10 09:34:25 +0100

    WIP: mapbox with fly to and filters

--- src/views/QuayMap.vue
+++ src/views/QuayMap.vue
@@ -1,35 +1,50 @@
 <template>
-  <mapbox
-    access-token="pk.eyJ1IjoianVyaWFuIiwiYSI6ImNqNTJlMTAzYzBmajEyd242ank0ZGppam8ifQ.JlOhR91CJQnc3U12pbMQwQ"
-    :map-options="mapOptions"
-    @map-load="loaded"
-  />
+  <div>
+    <div class="top-bar white"><top-bar /></div>
+    <mapbox
+      access-token="pk.eyJ1IjoianVyaWFuIiwiYSI6ImNqNTJlMTAzYzBmajEyd242ank0ZGppam8ifQ.JlOhR91CJQnc3U12pbMQwQ"
+      :map-options="mapOptions"
+      @map-load="loaded"
+      @map-init="initMap"
+    />
+  </div>
 </template>
 
 <script>
 import { mapGetters, mapState } from "vuex";
 import Mapbox from "mapbox-gl-vue";
+import TopBar from "../components/topBar.vue";
 
 export default {
   name: "Kaart",
-  components: { Mapbox },
+  components: { Mapbox, TopBar },
   computed: {
-    ...mapState(["profile", "quays"]),
+    ...mapState(["profile", "quays", "currentLocation"]),
     ...mapGetters(["filteredQuays"]),
     filterQuaysGeoJSON: function() {
       return {
+        type: "FeatureCollection",
+        features: this.filteredQuays.map((d) => ({
+          type: "Feature",
+          id: d.quaycode,
+          geometry: { type: "Point", coordinates: [d.geo.lon, d.geo.lat] },
+          properties: {
+            ...d,
+          },
+        })),
+      };
+    },
+    mapOptions: function() {
+      return {
+        style: "mapbox://styles/mapbox/light-v9",
+        center: this.currentLocation.slice().reverse(),
+        zoom: 14,
+      };
+    },
+    filterQuaysGeoJSONLayer: function() {
+      return {
         type: "geojson",
-        data: {
-          type: "FeatureCollection",
-          features: this.filteredQuays.map((d) => ({
-            type: "Feature",
-            id: d.quaycode,
-            geometry: { type: "Point", coordinates: [d.geo.lon, d.geo.lat] },
-            properties: {
-              ...d,
-            },
-          })),
-        },
+        data: this.filterQuaysGeoJSON,
       };
     },
   },
@@ -37,7 +52,7 @@ export default {
     loaded(map) {
       map.addLayer({
         id: "quays",
-        source: this.filterQuaysGeoJSON,
+        source: this.filterQuaysGeoJSONLayer,
         type: "circle",
         paint: {
           "circle-radius": 4,
@@ -47,15 +62,29 @@ export default {
         },
       });
     },
+    initMap(map) {
+      this.map = map;
+    },
+    refreshData() {
+      this.map.getSource("quays").setData(this.filterQuaysGeoJSON);
+    },
   },
   data: () => ({
-    test: false,
-    mapOptions: {
-      style: "mapbox://styles/mapbox/light-v9",
-      center: [4.9, 52.366667],
-      zoom: 12,
-    },
+    map: {},
   }),
+  watch: {
+    filteredQuays: function() {
+      this.refreshData();
+    },
+    currentLocation: function() {
+      this.map.flyTo({
+        center: this.currentLocation.slice().reverse(),
+      });
+    },
+  },
+  created() {
+    this.map = null; // here we define map as non-reactive
+  },
 };
 </script>
 
@@ -63,5 +92,10 @@ export default {
 #map {
   width: 100%;
   height: 100vh;
+}
+
+.top-bar {
+  // position: absolute;
+  width: 100%;
 }
 </style>
