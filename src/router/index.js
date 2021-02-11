b18184f92399097becc76d0ff97cbe6ae3627092
Jurian Baas <jurian.baas@gmail.com>
2021-02-11 16:20:16 +0100

    always scroll to top on router change

--- src/router/index.js
+++ src/router/index.js
@@ -45,6 +45,9 @@ const routes = [
 const router = new VueRouter({
   mode: "history",
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  },
 });
 
 export default router;
