50a1ab4578f038d5c149dd57230150c0d642fc34
Jurian Baas <jurian.baas@gmail.com>
2021-02-16 15:22:45 +0100

    added profile to quay filters

--- src/components/QuayFilters.vue
+++ src/components/QuayFilters.vue
@@ -2,9 +2,18 @@
   <div>
     <v-chip-group class="pl-4">
       <div class="ml-1 mt-2">
-        <v-chip pill @click="menu = !menu" color="secondary">
+        <v-chip
+          pill
+          @click="
+            vehicleMenu = !vehicleMenu;
+            profileMenu = false;
+            accessibilityMenu = false;
+          "
+          color="primary"
+          active-class="secondary"
+        >
           <v-icon left>
-            {{ menu ? "mdi-chevron-up" : "mdi-chevron-down" }}
+            {{ vehicleMenu ? "mdi-chevron-up" : "mdi-chevron-down" }}
           </v-icon>
           Type
           <img
@@ -24,44 +33,86 @@
             class="chip-icon ml-1"
             :src="require('@/assets/icons/vehicleFerryWhite.svg')"
         /></v-chip>
-      </div>
-      <div class="text-body-2 mt-n1 ml-2">
-        <v-switch
-          v-model="accessibleonlyFilter"
-          label="Alleen toegankelijk"
-          color="secondary"
-        ></v-switch>
+        <v-chip
+          pill
+          @click="
+            profileMenu = !profileMenu;
+            vehicleMenu = false;
+            accessibilityMenu = false;
+          "
+          color="primary"
+          active-class="secondary"
+        >
+          <v-icon left>
+            {{ profileMenu ? "mdi-chevron-up" : "mdi-chevron-down" }}
+          </v-icon>
+          <v-icon> mdi-account </v-icon>
+        </v-chip>
+        <v-chip
+          pill
+          @click="
+            accessibilityMenu = !accessibilityMenu;
+            vehicleMenu = false;
+            profileMenu = false;
+          "
+          color="primary"
+          active-class="secondary"
+        >
+          <v-icon left>
+            {{ accessibilityMenu ? "mdi-chevron-up" : "mdi-chevron-down" }}
+          </v-icon>
+          <v-icon> mdi-tune </v-icon>
+        </v-chip>
       </div>
     </v-chip-group>
+    <div v-if="accessibilityMenu" class="pl-5 py-6 pt-2 grey lighten-4">
+      <v-switch
+        v-model="accessibleonlyFilter"
+        label="Alleen toegankelijke haltes"
+        color="secondary"
+        hide-details
+      ></v-switch>
+    </div>
     <v-chip-group
+      v-if="vehicleMenu"
       v-model="vehicleFilter"
       multiple
       mandatory
-      class="pl-4"
-      v-if="menu"
+      class="pl-5 py-2 grey lighten-4"
     >
-      <v-chip filter :ripple="false" value="tram">
+      <v-chip filter :ripple="false" value="tram" class="secondary">
         Tram
       </v-chip>
-      <v-chip filter :ripple="false" value="bus">
+      <v-chip filter :ripple="false" value="bus" class="secondary">
         Bus
       </v-chip>
-      <v-chip filter :ripple="false" value="metro">
+      <v-chip filter :ripple="false" value="metro" class="secondary">
         Metro
       </v-chip>
-      <v-chip filter :ripple="false" value="ferry">
+      <v-chip filter :ripple="false" value="ferry" class="secondary">
         Pont
       </v-chip>
     </v-chip-group>
+
+    <template v-if="profileMenu">
+      <div class="px-3 grey lighten-4">
+        <h3 class="pa-2">Profiel</h3>
+        <profile type="inline" />
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
+import Profile from "@/components/Profile.vue";
 export default {
+  components: { Profile },
   name: "QuayFilters",
   data: () => ({
-    menu: false,
+    vehicleMenu: false,
+    profileMenu: false,
+    accessibilityMenu: false,
   }),
 
   computed: {
