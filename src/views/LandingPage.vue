d1a17e9b6e8523cc13ca2d761766c0a3f613b9f0
Jurian Baas <jurian.baas@gmail.com>
2021-03-17 18:00:17 +0100

    WIP: landing page with bottom callout

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="parent primary secondary">
     <div class="box white--text">
-      <div class="demo-callout-box" v-if="$vuetify.breakpoint.mdAndUp">
-        <img
-          :src="require('@/assets/demo-callout.svg')"
-          alt="pijltje naar demo"
-          class="demo-callout-image"
-        />
-      </div>
-      <div class="demo-callout-text">
-        <p class="title">
+      <div class="demo-callout-text title">
+        <p>
+          <img
+            :src="require('@/assets/haltebuddy-logo.svg')"
+            alt="logo haltebuddy"
+          />
+        </p>
+        <p class="mb-16">
           Met Haltebuddy weet je precies welke haltes er
           <strong>voor jou</strong> toegankelijk zijn. Vul je reisprofiel in, en
           zie precies waar je op moet letten voordat je de bus, tram, of metro
           in stapt!
         </p>
+        <!-- <p>
+          Met Haltebuddy weet je precies welke haltes er
+          <strong>voor jou</strong> toegankelijk zijn. Vul je reisprofiel in, en
+          zie precies waar je op moet letten voordat je de bus, tram, of metro
+          in stapt!
+        </p> -->
         <p v-if="$vuetify.breakpoint.xsOnly" class="white--text">
           Bekijk <a href="/">de demo op je mobiel</a>.
         </p>
+      </div>
+      <div class="demo-callout-box" v-if="$vuetify.breakpoint.mdAndUp">
+        <img
+          :src="require('@/assets/demo-callout-bottom.svg')"
+          alt="pijltje naar demo"
+          class="demo-callout-image"
+        />
       </div>
     </div>
     <div class="iframe-demo-box">
@@ -52,14 +64,13 @@ export default {
   text-align: right;
 }
 .demo-callout-image {
-  width: 175px;
+  width: 150px;
 }
 
 .demo-callout-text {
   // position: relative;
-  // top: 175px;
+  // top: 100px;
   padding-top: 50px;
-  margin-bottom: 96px;
 }
 
 .iframe-demo-box {
