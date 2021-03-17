c55ec8d0a617b49f233afc2d3835d87b8e9ee387
Jurian Baas <jurian.baas@gmail.com>
2021-03-17 11:25:25 +0100

    WIP: landing page nicely centered, secondary bg

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="parent pa-2">
-    <div class="box">
-      <p class="title">
+  <div class="parent primary secondary">
+    <div class="box white--text">
+      <div class="demo-callout-box">
+        <img
+          :src="require('@/assets/demo-callout.svg')"
+          alt="interactieve demo aanwijzer"
+          class="demo-callout-image"
+        />
+      </div>
+      <p class="title demo-callout-text">
         Met Haltebuddy weet je precies welke haltes er
         <strong>voor jou</strong> toegankelijk zijn. Vul je reisprofiel in, en
         zie precies waar je op moet letten voordat je de bus, tram, of metro in
         stapt!
       </p>
-      <p class="text-center text--secondary text-body-2">
+      <p class="text-center text-body-2 mt-16">
         meer weten?<br />
-        <v-icon x-large>mdi-chevron-down</v-icon>
+        <v-icon x-large color="white">mdi-chevron-down</v-icon>
       </p>
     </div>
     <div class="box">
@@ -39,6 +46,18 @@ export default {
   grid-template-columns: repeat(auto-fit, minmax(250px, 450px));
   place-items: center;
   justify-content: center;
-  height: 100vh;
+}
+
+.demo-callout-box {
+  position: relative;
+  text-align: right;
+  margin-top: 75px;
+}
+.demo-callout-image {
+  width: 175px;
+}
+
+.demo-callout-text {
+  padding-top: 96px;
 }
 </style>
