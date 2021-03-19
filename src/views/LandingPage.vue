7e72f0fe0dd3aa84354f01bfaa529e8e9fc6e53f
Jurian Baas <jurian.baas@gmail.com>
2021-03-19 15:10:53 +0100

    landingpage v1 🎉

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,40 +1,106 @@
 <template>
-  <div class="parent primary secondary">
-    <div class="box white--text">
-      <div class="demo-callout-text title">
-        <p>
+  <div>
+    <section class="hero-section primary secondary pa-2 white--text">
+      <div>
+        <div class="demo-callout-text title">
+          <!-- <p>
+            <img
+              :src="require('@/assets/haltebuddy-logo.svg')"
+              alt="logo haltebuddy"
+            />
+          </p> -->
+          <p class="mb-16">
+            Met Haltebuddy weet je precies welke haltes er
+            <strong>voor jou</strong> toegankelijk zijn. Vul je reisprofiel in,
+            en zie precies waar je op moet letten voordat je de bus, tram, of
+            metro in stapt!
+          </p>
+          <p v-if="$vuetify.breakpoint.xsOnly" class="white--text">
+            Bekijk <a href="/">de demo</a>.
+          </p>
+        </div>
+        <div class="demo-callout-box" v-if="$vuetify.breakpoint.mdAndUp">
           <img
-            :src="require('@/assets/haltebuddy-logo.svg')"
-            alt="logo haltebuddy"
+            :src="require('@/assets/demo-callout-bottom.svg')"
+            alt="pijltje naar demo"
+            class="demo-callout-image"
           />
-        </p>
-        <p class="mb-16">
-          Met Haltebuddy weet je precies welke haltes er
-          <strong>voor jou</strong> toegankelijk zijn. Vul je reisprofiel in, en
-          zie precies waar je op moet letten voordat je de bus, tram, of metro
-          in stapt!
-        </p>
-        <!-- <p>
-          Met Haltebuddy weet je precies welke haltes er
-          <strong>voor jou</strong> toegankelijk zijn. Vul je reisprofiel in, en
-          zie precies waar je op moet letten voordat je de bus, tram, of metro
-          in stapt!
-        </p> -->
-        <p v-if="$vuetify.breakpoint.xsOnly" class="white--text">
-          Bekijk <a href="/">de demo op je mobiel</a>.
-        </p>
+        </div>
       </div>
-      <div class="demo-callout-box" v-if="$vuetify.breakpoint.mdAndUp">
-        <img
-          :src="require('@/assets/demo-callout-bottom.svg')"
-          alt="pijltje naar demo"
-          class="demo-callout-image"
-        />
+      <div class="iframe-demo-box">
+        <IPhoneX v-if="$vuetify.breakpoint.smAndUp" />
       </div>
-    </div>
-    <div class="iframe-demo-box">
-      <IPhoneX v-if="$vuetify.breakpoint.smAndUp" />
-    </div>
+    </section>
+    <section class="explanation-section primary pa-2 white--text">
+      <div class="mt-8 explanation-section-text">
+        <div class="title">
+          <p>
+            <img
+              :src="require('@/assets/haltebuddy-logo.svg')"
+              alt="logo haltebuddy"
+            />
+          </p>
+          <p>
+            Haltebuddy is een prototype van het focusteam van het innovatieteam
+            van de gemeente Amsterdam, in samenwerking met de VRA en de GVB.
+          </p>
+          <p>
+            We hebben Haltebuddy ontwikkeld om aan te tonen dat reisadvies voor
+            mensen met een beperking persoonlijker kan. Dit doen we door een
+            reisprofiel te koppelen aan de informatie in het Centraal
+            Haltebestand. Het experiment Haltebuddy is onderdeel van het
+            uitvoeringsprogramma inclusieve mobiliteit.
+          </p>
+        </div>
+        <div class="mt-8">
+          <p>
+            <v-btn
+              color="secondary"
+              depressed
+              class="text-none text-body rounded-0"
+              x-large
+              href="https://docs.google.com/presentation/d/18z1kKqbTW5bTGFxQfOwhL4FDkDlxv4yhTpL-jijnobo/edit?usp=sharing"
+              target="_blank"
+            >
+              <v-icon left dark>
+                mdi-download
+              </v-icon>
+              <strong>Download de evaluatie</strong>
+            </v-btn>
+          </p>
+          <p>
+            <v-btn
+              color="secondary"
+              depressed
+              class="text-none text-body rounded-0"
+              x-large
+              href="https://github.com/jurb/haltebuddy"
+              target="_blank"
+            >
+              <v-icon left dark>
+                mdi-github
+              </v-icon>
+              <strong>Bekijk de broncode op Github</strong>
+            </v-btn>
+          </p>
+          <p>
+            <v-btn
+              color="secondary"
+              depressed
+              class="text-none text-body rounded-0"
+              x-large
+              href="https://observablehq.com/@jurb/haltebuddy-aanbevelingen-techniek"
+              target="_blank"
+            >
+              <v-icon left dark>
+                mdi-robot
+              </v-icon>
+              <strong>Meer over de techniek</strong>
+            </v-btn>
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -51,16 +117,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.parent {
+.hero-section {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 450px));
+  grid-template-columns: repeat(auto-fit, minmax(300px, 500px));
   place-items: center;
   justify-content: center;
 }
 
+.explanation-section {
+}
+
+.explanation-section-text {
+  margin: auto;
+  max-width: 768px;
+}
+
 .demo-callout-box {
-  // position: relative;
-  // top: 125px;
   text-align: right;
 }
 .demo-callout-image {
@@ -68,8 +140,6 @@ export default {
 }
 
 .demo-callout-text {
-  // position: relative;
-  // top: 100px;
   padding-top: 50px;
 }
 
