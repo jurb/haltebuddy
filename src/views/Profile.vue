5168d09bb275c6925507c98cde85264334f83760
Jurian Baas <jurian.baas@gmail.com>
2021-04-01 11:12:06 +0200

    move profile intro text to view

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,14 @@
 <template>
-  <profile />
+  <div>
+    <div class="pa-4">
+      <h3>Reisprofiel</h3>
+      <p>
+        Vul hieronder jouw reisprofiel in om te zien welke haltes voor jou
+        toegankelijk zijn.
+      </p>
+    </div>
+    <profile />
+  </div>
 </template>
 
 <script>
