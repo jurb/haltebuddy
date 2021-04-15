0b4e0619cd31fb9f621893ec076434dcac00d539
Jurian Baas <jurian.baas@gmail.com>
2021-04-15 11:42:13 +0200

    longer interval

--- src/views/HalteknopLanding.vue
+++ src/views/HalteknopLanding.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-container class="pa-0 mb-n4">
-      <v-carousel height="675" cycle interval="3000" show-arrows-on-hover>
+      <v-carousel height="675" cycle interval="4000" show-arrows-on-hover>
         <v-carousel-item src="@/assets/halteknop01.png"></v-carousel-item>
         <v-carousel-item src="@/assets/halteknop02.png"></v-carousel-item>
         <v-carousel-item src="@/assets/halteknop03.png"></v-carousel-item>
